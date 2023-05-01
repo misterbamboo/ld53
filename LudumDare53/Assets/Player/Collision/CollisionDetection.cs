c88e8396adeb1312c0950148d890e00bacfc2527
armenbalian <raphaelfortin@hotmail.com>
2023-05-01 19:44:39 -0400

    crash sound

--- LudumDare53/Assets/Player/Collision/CollisionDetection.cs
+++ LudumDare53/Assets/Player/Collision/CollisionDetection.cs
@@ -6,7 +6,16 @@ public class CollisionDetection : MonoBehaviour
 {
     ParticleSystem particle;
 
-    private void Awake()
+    [SerializeField] AudioSource crashAudioSource;
+
+    private IGameState gameState;
+
+    void Start()
+    {
+        gameState = GameManager.GetGameState();
+    }
+
+    void Awake()
     {
         particle = GetComponentInChildren<ParticleSystem>();
         particle.Stop();
@@ -19,9 +28,21 @@ public class CollisionDetection : MonoBehaviour
             return;
         }
 
+
+
+
+        // Map the total speed to a volue range
+        float volume = Mathf.Lerp(0.01f, 0.05f, Mathf.InverseLerp(0.0f, 80.0f, gameState.GetSpeed()));
+
+        // Update the pitch of the audio source
+        crashAudioSource.volume = (volume);
+
+
+
         // TODO : ADD COLISION SOUND
         particle.gameObject.transform.position = other.ClosestPointOnBounds(transform.position);
-        particle.Play();        
+        particle.Play();
+        crashAudioSource.Play();
     }
 
     private void OnTriggerExit(Collider other)
